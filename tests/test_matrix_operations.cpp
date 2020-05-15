9bcc37d57b0bac75015d918b74c984b537d9748a
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-05-15 11:40:51 +0300

    Tests added

--- tests/test_matrix_operations.cpp
+++ tests/test_matrix_operations.cpp
@@ -337,3 +337,205 @@ TEST(inverse_test, unexpected_args){
 //    ASSERT_TRUE( identity(3)->equal(multiply_matrix(Q_in,Q), 0.1) );
 //    ASSERT_TRUE(is_upper(R));
 //}
+
+//TEST(isUpper_test, validMatrix){
+//    Matrix m_1 = Matrix(3,3, {4, 3, 2,
+//                              0, 4,-1,
+//                              0, 0, 4});
+//
+//    Matrix m_2 = Matrix(2, 2, {4, 3,
+//                               0, 4});
+//
+//    Matrix m_4 = Matrix(2, 2, {0, 0,
+//                               0, 0});
+//
+//    Matrix m_3 = Matrix(4,4, {4, 3, 2, 6,
+//                              0, 4,-1, 8,
+//                              0, 0,-1, 8,
+//                              0, 0, 0, 4});
+//
+//    ASSERT_TRUE(is_upper(&m_1));
+//    ASSERT_TRUE(is_upper(&m_2));
+//    ASSERT_TRUE(is_upper(&m_3));
+//    ASSERT_TRUE(is_upper(&m_4));
+//}
+//
+//TEST(isUpper_test, invalidMatrix){
+//    Matrix m_1 = Matrix(3,3, {4, 3, 2,
+//                              0, 4,-1,
+//                              0, 1, 4});
+//
+//    Matrix m_2 = Matrix(2, 2, {4, 3,
+//                               1, 4});
+//
+//    Matrix m_3 = Matrix(4,4, {4, 3, 2, 6,
+//                              1, 4,-1, 8,
+//                              0, 0,-1, 8,
+//                              0, 0, 0, 4});
+//
+//    Matrix m_4 = Matrix(3,2, {4, 3, 2,
+//                              0, 4,-1});
+//
+//    Matrix m_5 = Matrix(1,6, {4, 3, 2,
+//                              0, 4,-1});
+//
+//    ASSERT_FALSE(is_upper(&m_1));
+//    ASSERT_FALSE(is_upper(&m_2));
+//    ASSERT_FALSE(is_upper(&m_3));
+//    ASSERT_FALSE(is_upper(&m_4));
+//    ASSERT_FALSE(is_upper(&m_5));
+//}
+//
+//TEST(traceTest, validCases){
+//    Matrix m_1 = Matrix(3,3, {4, 3, 2,
+//                              0, 4,-1,
+//                              5, 0, 4});
+//
+//    Matrix m_2 = Matrix(2, 2, {4, 3,
+//                               0, 4});
+//
+//    Matrix m_3 = Matrix(4,4, {0, 3, 2, 6,
+//                              0, 0,-1, 8,
+//                              0, 0, 0, 8,
+//                              0, 4, 0, 0});
+//
+//    Matrix m_4 = Matrix(2, 2, {0, 0,
+//                               0, 0});
+//
+//    Matrix m_5 = Matrix(1, 1, {1});
+//
+//    ASSERT_EQ(trace(&m_1), 12);
+//    ASSERT_EQ(trace(&m_2), 8);
+//    ASSERT_EQ(trace(&m_3), 0);
+//    ASSERT_EQ(trace(&m_4), 0);
+//    ASSERT_EQ(trace(&m_5), 1);
+//}
+//
+//TEST(traceTest, invalidCases){
+//    Matrix m_1 = Matrix(3,2, {4, 3, 2,
+//                              5, 0, 4});
+//
+//    Matrix m_2 = Matrix(2, 6, {4, 3,
+//                               0, 4, 0, 4, 0, 4, 0, 4, 0, 4});
+//
+//    Matrix m_3 = Matrix(4,5, {0, 3, 2, 6,
+//                              0, 0,-1, 8,
+//                              0, 0,-1, 8,
+//                              0, 0, 0, 8,
+//                              0, 4, 0, 0});
+//
+//    Matrix m_4 = Matrix(3, 2, {0, 0, 0, 0,
+//                               0, 0});
+//
+//    ASSERT_EQ(trace(&m_1), -1);
+//    ASSERT_EQ(trace(&m_2), -1);
+//    ASSERT_EQ(trace(&m_3), -1);
+//    ASSERT_EQ(trace(&m_4), -1);
+//}
+//
+//TEST(eigenValuesTest, generalTest){
+//    Matrix m_1 = Matrix(5, 5, {
+//            1, 2, 3, 4, 5,
+//            6, 7, 8, 9, 0,
+//            5, 4, 3, 2, 1,
+//            2, 2, 5, 6, 0,
+//            1, 5, 8, 2, 9
+//    });
+//    //inf loop
+//    std::vector<double> res_1 = {1.923, 6.011, 19.150};
+//
+//    Matrix m_2 = Matrix(3, 3, {
+//            1, 2, 3,
+//            6, 7, 8,
+//            5, 4, 3
+//    });
+//    //fail (far from true result)
+//    std::vector<double> res_2 = {-2.13217, 13.13217};
+//
+//
+//    Matrix m_3 = Matrix(2, 2, {
+//            1, 2,
+//            9, 4
+//    });
+//    //ok
+//    std::vector<double> res_3 = {-2, 7};
+//
+//    Matrix m_4 = Matrix(3, 3, {
+//            1, 2, 5,
+//            9, 4, 0,
+//            0, 2, 0
+//    });
+//    //inf loop
+//    std::vector<double> res_4 = {8.100};
+//
+//    Matrix m_5 = Matrix(3, 3, {
+//            0, 0, 0,
+//            9, 4, 0,
+//            0, 2, 0
+//    });//bug (-nan)
+//    std::vector<double> res_5 = {0, 4};
+//
+//    Matrix m_6 = Matrix(3, 3, {
+//            0, 2, 1,
+//            9, 4, 0,
+//            1, 2, 0
+//    });//almost bug (-0.999999 instead of -1)
+//    std::vector<double> res_6 = {-2, -1, 7};
+//
+//    std::vector<Matrix> matrices = {m_1, m_2, m_3, m_4, m_5, m_6};
+//    std::vector<std::vector<double>> resVectors = {res_1, res_2, res_3, res_4, res_4, res_5, res_6};
+//
+//    for (auto g = 0; g < matrices.size(); ++g)
+//    {
+//        std::vector<double> localRes = qr_method_eigenvalues(&matrices[g]);
+//        sort(localRes.begin(), localRes.end());
+//        ASSERT_EQ(localRes.size(), resVectors[g]);
+//        for (auto i = 0; i < localRes.size(); ++i)
+//        {
+//            for (auto j = 0; j < resVectors.size(); ++j)
+//            {
+//                ASSERT_DOUBLE_EQ(localRes[j], resVectors[g][j]);
+//            }
+//        }
+//    }
+//}
+//
+//TEST(eigenVectors, generalTest){
+//    Matrix m_1 = Matrix(3, 3, {
+//            1, 0, 0,
+//            9, 1, 0,
+//            1, 2, 1});
+//    std::vector<double> vals_1 = {1};
+//    //notinv
+//    Matrix vect_1 = Matrix(3, 1, {0, 0, 1});
+//
+//    Matrix m_2 = Matrix(3, 3, {
+//            1, 0, 1,
+//            1, 2, 1,
+//            1, 1, 1});
+//    std::vector<double> vals_2 = {0, 1, 3};
+//    //notinv
+//    Matrix vect_2 = Matrix(3, 3, {-1, 0, 1,
+//                                  -1, 1, 0,
+//                                  0.5, 1.5, 1});
+//
+//    Matrix m_3 = Matrix(3, 3, {
+//            1, 0, 1,
+//            1, 1, 1,
+//            0, 1, 0
+//    });
+//    std::vector<double> vals_3 = {0, 2};
+//    //notinv
+//    Matrix vect_3 = Matrix(3, 2, {-1, 0, 1,
+//                                  1, 2, 1});
+//
+//    std::vector<Matrix> mat = {m_1, m_2, m_3};
+//    std::vector<std::vector<double>> val = {vals_1, vals_2, vals_3};
+//    std::vector<Matrix> vect = {vect_1, vect_2, vect_3};
+//
+//    for(auto i = 0; i < mat.size(); ++i)
+//    {
+//        ASSERT_EQ(eigen_vectors(&mat[i], val[i]), vect[i]);
+//    }
+//}
+//
